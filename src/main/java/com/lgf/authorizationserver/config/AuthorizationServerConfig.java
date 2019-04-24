0765174938b1587126f0942973f0d88aaed844f9
Lucas Favaro <lucas.favaro@turner.com>
2019-04-24 17:39:16 -0300

    STABLE

--- src/main/java/com/lgf/authorizationserver/config/AuthorizationServerConfig.java
+++ src/main/java/com/lgf/authorizationserver/config/AuthorizationServerConfig.java
@@ -1,11 +1,14 @@
 package com.lgf.authorizationserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
@@ -17,30 +20,43 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@SuppressWarnings("deprecation")
 	@Bean
 	public static NoOpPasswordEncoder passwordEncoder() {
-	return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	}
+
+	/** WORKARROUND
+	 * SE AGREGA ESTO PARA PODER PEDIR EL TOKEN CON GRANT TYPE PASSWORD 
+	 *  https://github.com/spring-projects/spring-security-oauth/issues/1328
+	 * **/
+	
+	private AuthenticationManager authenticationManagerBean;
+
+	@Autowired
+	public void setAuthenticationManagerBean(AuthenticationManager authenticationManagerBean) {
+		this.authenticationManagerBean = authenticationManagerBean;
 	}
 	
 	@Override
-	public void configure(ClientDetailsServiceConfigurer clients) 
-		      throws Exception {
-		        clients.inMemory()
-		          .withClient("support")
-		          .secret("support")
-		          .authorizedGrantTypes("password","authorization_code", "refresh_token","client_credentials")
-		          .scopes("read");
-		    }
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+		endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManagerBean);
+	}
+	/** FIN WORKARROUND **/ 
 	
-	
-    @Bean
-    public TokenStore tokenStore() {
 
-        return new InMemoryTokenStore();
-    }
-    
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception
-    {
-       oauthServer.checkTokenAccess("permitAll()");    
-    }
-    
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("tracker-support").secret("tracker-support")
+				.authorizedGrantTypes("password", "authorization_code", "refresh_token", "client_credentials")
+				.scopes("home");
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+		oauthServer.checkTokenAccess("isAuthenticated()");
+	}
+
 }
--- src/main/java/com/lgf/authorizationserver/config/WebSecurityConfig.java
+++ src/main/java/com/lgf/authorizationserver/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.lgf.authorizationserver.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,11 +13,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+	
+	@Override
 	  public void configure(AuthenticationManagerBuilder builder)
 	          throws Exception {
 	      builder.inMemoryAuthentication()
 	             .withUser("lfavaro")
 	             .password("lfavaro")
-	             .roles("ADMIN");
+	             .roles("TALL");
 	  }
 }
